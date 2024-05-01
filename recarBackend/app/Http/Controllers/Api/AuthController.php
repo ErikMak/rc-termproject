0b6b73f22947aac5f11e25c67469ba0c41cd15cd
ErikMak <47Poligons@gmail.com>
2024-05-01 17:42:42 +0300

    Система авторизации и регистрации

---
+++ recarBackend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends BaseController
+{
+    public function login(Request $request) {
+        $data = $request->only('login', 'password');
+
+        // Создание токена
+        if($token = Auth::attempt($data)) {
+            return $this->sendOK()->header('Authorization', $token);
+        }
+
+        return $this->sendError('Отказ входа!');
+    }
+
+    public function register(Request $request) {
+        $user = User::create([
+            'login' => $request->login,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return $this->sendOK();
+    }
+
+    public function logout() {
+        Auth::logout();
+
+        return $this->sendOK();
+    }
+
+    public function user(Request $request) {
+        $user = User::find(Auth::user()->id);
+
+        return $this->sendResponse($user);
+    }
+
+    public function refresh() {
+        if($token = Auth::refresh()) {
+            return $this->sendOK()->header('Authorization', $token);
+        }
+
+        return $this->sendError('Ошибка обновления токена авторизации!', 401);
+    }
+}
--- recarBackend/app/Models/User.php
+++ recarBackend/app/Models/User.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'login', 'password', 'role'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
