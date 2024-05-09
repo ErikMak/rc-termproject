8ae6be522a9cd1f13a300910fe79fbc8b08ca8f2
ErikMak <47Poligons@gmail.com>
2024-05-09 23:41:01 +0300

    Правки перед развертыванием

--- recarFrontend/src/components/Header/HeaderComp.vue
+++ recarFrontend/src/components/Header/HeaderComp.vue
@@ -15,11 +15,19 @@
     </div>
     <!--  Аватарка  -->
     <template v-slot:append>
-      <v-avatar class="me-1">
+      <v-avatar v-if="getLoggedStatus" class="me-1">
         <v-img
             :src="avatar"
         ></v-img>
       </v-avatar>
+      <v-btn v-else
+          to="auth"
+         color="white"
+         variant="tonal"
+             size="small"
+      >
+        Войти
+      </v-btn>
     </template>
   </v-app-bar>
 </template>
@@ -29,13 +37,23 @@ import { defineComponent } from 'vue';
 import gradient from "@/assets/gradient.webp"
 import logo from "@/assets/logo2.png"
 import avatar from "@/assets/img_avatar.webp"
+import {mapActions, mapGetters} from "vuex";
 export default defineComponent({
   name: 'HeaderComponent',
   data: () => ({
     gradient: gradient,
     avatar: avatar,
     logo: logo
-  })
+  }),
+  methods: {
+    ...mapActions(["checkLoggedStatus"])
+  },
+  computed: {
+    ...mapGetters(["getLoggedStatus"])
+  },
+  created() {
+    this.checkLoggedStatus()
+  }
 });
 </script>
 
