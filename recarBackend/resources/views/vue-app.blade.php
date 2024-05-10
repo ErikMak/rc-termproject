76987628bc9c2fcf73af4440f080294f3ff16b43
ErikMak <47Poligons@gmail.com>
2024-05-10 13:00:53 +0300

    Правки на деплой

--- recarBackend/resources/views/vue-app.blade.php
+++ recarBackend/resources/views/vue-app.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite App</title>
-    <script type="module" crossorigin src="/assets/index-Bmvq8wfM.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-D7bZn8E8.css">
+    <script type="module" crossorigin src="/assets/index-CvDqB7Dg.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-fDf2iRU4.css">
   </head>
   <body>
     <div id="app"></div>
